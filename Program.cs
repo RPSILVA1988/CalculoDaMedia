107096c0181bc1df1d214df9f728c1223f6cd84a
RANIELE SILVA <comraniele@hotmail.com>
2021-03-07 18:46:44 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CalculoDaMedia
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//media de dois numeros
+			double x, y, media;
+
+			Console.Write("Digite o primeiro numero: ");
+			x = double.Parse(Console.ReadLine());
+			Console.Write("Digite o segundo numero: ");
+			y = double.Parse(Console.ReadLine());
+			media = (x + y) / 2.0;
+			Console.WriteLine("Media = " + media);
+
+		}
+	}
+}
